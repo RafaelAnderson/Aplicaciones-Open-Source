17813bc9e4d3dbab83dc85770fd8e8ff8f5d8fac
Crhistianz <crhistianzea@hotmail.com>
2020-05-03 15:08:03 -0500

    Agregar clases ClientDto y CardDto

---
+++ Dto/ClientDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointFood.Dto
+{
+    public class ClientDto
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    public class ClientCreateDto
+    {
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    public class ClientUpdateDto
+    {
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
