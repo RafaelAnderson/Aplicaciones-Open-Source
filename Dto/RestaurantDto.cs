98abe8b4322319003f2e965efa791f025574e886
Crhistianz <crhistianzea@hotmail.com>
2020-05-03 18:50:23 -0500

    Agregar clases RestaurantDto y RestaurantOwnerDto

---
+++ Dto/RestaurantDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointFood.Dto
+{
+    public class RestaurantDto
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string RestaurantOwnerName { get; set; }
+    }
+
+    public class RestaurantCreateDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int RestauranteOwnerId { get; set; }
+    }
+
+    public class RestaurantUpdateDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Dto/RestaurantOwnerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointFood.Dto
+{
+    public class RestaurantOwnerDto
+    {
+        public int RestaurantOwnerId { get; set; }
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class RestaurantOwnerCreateDto
+    {
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class RestaurantOwnerUpdateDto
+    {
+        public string Name { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+    }
+}
