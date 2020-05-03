068906f3743464794c5c7536150b0be90f6f164f
Crhistianz <crhistianzea@hotmail.com>
2020-05-02 21:11:15 -0500

    Agregar clases en Model y Persistence para generar base de datos

---
+++ ConfigMapper/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PointFood.Commons;
+using PointFood.Dto;
+using PointFood.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointFood.ConfigMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Client, ClientDto>();
+            CreateMap<DataCollection<Client>, DataCollection<ClientDto>>();
+        }
+    }
+}
